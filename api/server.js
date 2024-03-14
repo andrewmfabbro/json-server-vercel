dbd8642111e0b09e6437ee60a57ed7ed882dbe7b
Andrew <andrewmfabbro@gmail.com>
2024-03-14 16:36:58 -0400

    Update server.js add /tmp/

--- api/server.js
+++ api/server.js
@@ -6,10 +6,12 @@ const server = jsonServer.create()
 // Uncomment to allow write operations
  const fs = require('fs')
  const path = require('path')
- const filePath = path.join('db.json')
+ //const filePath = path.join('db.json')
+ const filePath = path.join("/tmp", "db.json"); //add tmp path to fix routing
  const data = fs.readFileSync(filePath, "utf-8");
  const db = JSON.parse(data);
  const router = jsonServer.router(db)
+
 
 // Comment out to allow write operations
 const router = jsonServer.router('db.json')
